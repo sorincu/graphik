704c6b380bb069d95ffc59ed33821f89ae8bf4a5
sorincu <scuibari1@gmail.com>
2022-06-22 19:29:41 +0100

    2 files modified

--- routes/Popular.tsx
+++ routes/Popular.tsx
@@ -1,10 +1,27 @@
 import React = require('react');
 import styles from '../css/Popular.module.css';
 
+// data
+import articles from '../data/articles';
+
 function Popular() {
   return (
     <div className={styles.content}>
       <h1 className={`${styles.header} border-bottom`}>Popular</h1>
+
+      {articles.map((article) => (
+        <div className={styles.article}>
+          <img
+            src={article.previewImg}
+            className={styles.img}
+            alt="article image"
+          />
+
+          <div className={styles.articleTextDiv}>
+            <p>{article.title}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
