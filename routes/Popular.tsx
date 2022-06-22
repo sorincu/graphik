7dbf18fe1241392ee096d9156854a811a942c7a6
sorincu <scuibari1@gmail.com>
2022-06-22 20:23:54 +0100

    2 files modified

--- routes/Popular.tsx
+++ routes/Popular.tsx
@@ -9,19 +9,32 @@ function Popular() {
     <div className={styles.content}>
       <h1 className={`${styles.header} border-bottom`}>Popular</h1>
 
-      {articles.map((article) => (
-        <div className={styles.article}>
-          <img
-            src={article.previewImg}
-            className={styles.img}
-            alt="article image"
-          />
+      <div className={styles.articlesAdvertContainer}>
+        <div className={styles.articlesContainer}>
+          {articles.map((article) => (
+            <div className={`${styles.article}`}>
+              <img
+                src={article.previewImg}
+                className={styles.img}
+                alt="article image"
+              />
 
-          <div className={styles.articleTextDiv}>
-            <p>{article.title}</p>
-          </div>
+              <div className={styles.articleTextDiv}>
+                <h2>{article.title}</h2>
+                <p>{article.description}</p>
+                <div className={styles.authorDateDiv}>
+                  <p>{article.author}</p>
+                  <p>{article.publicationDate}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+
+        <div className={styles.ad}>
+          <h1>ADVERTISEMENT</h1>
+        </div>
+      </div>
     </div>
   );
 }
