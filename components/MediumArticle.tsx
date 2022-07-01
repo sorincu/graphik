b37ead006ef97bc8f4ba294d63cc795c5774733c
sorincu <scuibari1@gmail.com>
2022-07-01 09:44:31 +0100

    Added routing to articles

--- components/MediumArticle.tsx
+++ components/MediumArticle.tsx
@@ -1,27 +1,31 @@
 import React = require('react');
 import styles from '../css/MediumArticle.module.css';
 
+import { Link } from 'react-router-dom';
+
 // data
 import articles from '../data/articles';
 
 function MediumArticle(props) {
   return (
-    <div className={`${props.feature} ${styles.article}`}>
-      <img className={props.imgWidth} src={articles[0].previewImg} />
+    <Link to={`/articles/${props.articleId}`}>
+      <div className={`${props.feature} ${styles.article}`}>
+        <img className={props.imgWidth} src={articles[0].previewImg} />
 
-      <div className={styles.categoryDateContainer}>
-        <p className={styles.category}>{articles[0].category}</p>
-        <p className={styles.date}>{articles[0].publicationDate}</p>
+        <div className={styles.categoryDateContainer}>
+          <p className={styles.category}>{articles[0].category}</p>
+          <p className={styles.date}>{articles[0].publicationDate}</p>
+        </div>
+
+        <h2 className={styles.title}>{articles[0].title}</h2>
+
+        {props.withContent && (
+          <p className={styles.description}>{articles[0].description}</p>
+        )}
+
+        <p className={styles.author}>{articles[0].author}</p>
       </div>
-
-      <h2 className={styles.title}>{articles[0].title}</h2>
-
-      {props.withContent && (
-        <p className={styles.description}>{articles[0].description}</p>
-      )}
-
-      <p className={styles.author}>{articles[0].author}</p>
-    </div>
+    </Link>
   );
 }
 
