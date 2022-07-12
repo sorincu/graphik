f15efc3e3aa7af957e3d14d0a71d6c0969d523c5
sorincu <scuibari1@gmail.com>
2022-07-12 22:37:27 +0100

    Tested menu component

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -2,13 +2,13 @@ import React = require('react');
 
 export default function Menu(props) {
   return (
-    <>
+    <div>
       {props.showMenu && (
         <div>
           <h1>Hello</h1>
           <h2>World</h2>
         </div>
       )}
-    </>
+    </div>
   );
 }
