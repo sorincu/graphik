5eccfe477b3a73d3910f363a82be12bc89f20845
sorincu <scuibari1@gmail.com>
2022-07-13 22:21:43 +0100

    Added categories to the menu tab

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -3,13 +3,17 @@ import React = require('react');
 // styles
 import styles from '../css/Menu.module.css';
 
+// data
+import categories from '../data/categories.tsx';
+
 export default function Menu(props) {
   return (
     <div>
       {props.showMenu && (
         <div className={styles.menuWrapper}>
-          <h1>Hello</h1>
-          <h2>World</h2>
+          {categories.map((category) => (
+            <p>{category.type} </p>
+          ))}
         </div>
       )}
     </div>
